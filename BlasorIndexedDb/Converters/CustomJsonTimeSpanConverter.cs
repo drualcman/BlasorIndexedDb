7fc662023d1e9f784fcfead6a5914bb21c823789
Sergi Ortiz Gómez <drualcman@msn.com>
2024-03-28 08:30:40 +0800

    update customjsontimespanconverter to avoid return exceptions

--- BlasorIndexedDb/Converters/CustomJsonTimeSpanConverter.cs
+++ BlasorIndexedDb/Converters/CustomJsonTimeSpanConverter.cs
@@ -3,52 +3,49 @@ internal class CustomJsonTimeSpanConverter : JsonConverter<TimeSpan>
 {
     public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType != JsonTokenType.StartObject)
+        if (reader.TokenType == JsonTokenType.StartObject)
         {
-            throw new JsonException("Expected StartObject token type.");
-        }
+            long? ticks = null;
+            int days = 0, hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
 
-        long? ticks = null;
-        int days = 0, hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
-
-        while (reader.Read())
-        {
-            if (reader.TokenType == JsonTokenType.EndObject)
+            while (reader.Read())
             {
-                return ticks.HasValue ? new TimeSpan(ticks.Value) : new TimeSpan(days, hours, minutes, seconds, milliseconds);
-            }
-
-            if (reader.TokenType == JsonTokenType.PropertyName)
-            {
-                string propertyName = reader.GetString();
-                reader.Read();
-                switch (propertyName)
+                if (reader.TokenType == JsonTokenType.EndObject)
                 {
-                    case "Ticks":
-                        ticks = reader.GetInt64();
-                        break;
-                    case "Days":
-                        days = reader.GetInt32();
-                        break;
-                    case "Hours":
-                        hours = reader.GetInt32();
-                        break;
-                    case "Minutes":
-                        minutes = reader.GetInt32();
-                        break;
-                    case "Seconds":
-                        seconds = reader.GetInt32();
-                        break; 
-                    case "Milliseconds":
-                        milliseconds = reader.GetInt32();
-                        break; 
-                    default:
-                        break;
+                    return ticks.HasValue ? new TimeSpan(ticks.Value) : new TimeSpan(days, hours, minutes, seconds, milliseconds);
+                }
+
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    string propertyName = reader.GetString();
+                    reader.Read();
+                    switch (propertyName)
+                    {
+                        case "Ticks":
+                            ticks = reader.GetInt64();
+                            break;
+                        case "Days":
+                            days = reader.GetInt32();
+                            break;
+                        case "Hours":
+                            hours = reader.GetInt32();
+                            break;
+                        case "Minutes":
+                            minutes = reader.GetInt32();
+                            break;
+                        case "Seconds":
+                            seconds = reader.GetInt32();
+                            break;
+                        case "Milliseconds":
+                            milliseconds = reader.GetInt32();
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
         }
-
-        throw new JsonException("JSON is not a valid TimeSpan format.");
+        return new TimeSpan();
     }
 
     public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
