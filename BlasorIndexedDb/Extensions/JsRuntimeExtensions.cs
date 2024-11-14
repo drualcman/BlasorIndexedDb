cccaa6c71e833e36a0890784fa3ddfcd6b8eb3d8
Sergi Ortiz Gomez <drualcman@gmail.com>
2024-11-14 09:27:10 +0800

    Upgrade to NET9

--- BlasorIndexedDb/Extensions/JsRuntimeExtensions.cs
+++ BlasorIndexedDb/Extensions/JsRuntimeExtensions.cs
@@ -1,8 +1,10 @@
 ﻿namespace BlazorIndexedDb.Extensions;
 internal static class JsRuntimeExtensions
 {
+#nullable enable
     public static async ValueTask<T> GetJsonResult<T>(this IJSObjectReference jsObjectReference, string identifier, params object?[]? args)
     {
+#nullable disable
         JsonElement element = await jsObjectReference.InvokeAsync<JsonElement>(identifier, args);
         return JsonSerializer.Deserialize<T>(element, new JsonSerializerOptions
         {
